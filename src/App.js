68a762e2c045d51a8cb956e8b78f57860c873f74
Nick DeChane <ndechane@gmail.com>
2022-08-11 20:59:28 -0400

    async campsites

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import React from "react";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import "./App.css";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+}, [dispatch]);
+
   return (
     <div className="App">
       <Header />
